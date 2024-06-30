8df7ed5f232a63815d2471bbb43ab6326adb05fd
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-06-30 17:30:52 +0200

    feat: add swedish content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,13 +10,13 @@ export default function Home() {
             <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
               <div className="flex flex-col justify-center space-y-4">
                 <div className="space-y-2">
-                  <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
-                    Streamline Your Web Development
+                  <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none pb-2">
+                    Se vilka politiker som lever upp till sina ord
                   </h1>
                   <p className="max-w-[600px] text-muted-foreground md:text-xl">
-                    Unlock your team's full potential with our powerful web
-                    development platform. Effortlessly build, deploy, and scale
-                    your applications.
+                    Brutna Löften är en databas som spårar och analyserar
+                    politiska löften. Vi håller koll på vad politiker säger och
+                    vad de faktiskt gör.
                   </p>
                 </div>
                 <div className="flex flex-col gap-2 min-[400px]:flex-row">
@@ -25,7 +25,7 @@ export default function Home() {
                     className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
                     prefetch={false}
                   >
-                    Get Started
+                    Sök i databasen
                   </Link>
                 </div>
               </div>
