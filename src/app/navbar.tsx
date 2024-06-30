fae4f7730f7907dc5f6207ad362d14827af27665
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-06-30 20:57:56 +0200

    feaT: add basic menu

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,23 +1,26 @@
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>Link</NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <header className="bg-background border-b shadow-sm">
+      <div className="container px-4 md:px-6 flex items-center h-14 md:h-16">
+        <Link
+          href="/"
+          className="font-bold text-lg md:text-xl"
+          prefetch={false}
+        >
+          Brutna Löften
+        </Link>
+        {/* <nav className="flex items-center gap-4 md:gap-6 ml-6">
+          <Link
+            href="#"
+            className="text-muted-foreground hover:text-foreground transition-colors"
+            prefetch={false}
+          >
+            Sök
+          </Link>
+        </nav> */}
+      </div>
+    </header>
   );
 }
