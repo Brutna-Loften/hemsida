06fe780b65fc33ef0a3a6b5559ba382f012e38c8
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-06-30 21:28:42 +0200

    chore: rename component to searchjj

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,7 @@
 import "@/app/globals.css";
 import { Input } from "@/components/ui/input";
 
-export default function Home() {
+export default function Search() {
   return (
     <div className="container pt-4 px-4 md:px-6">
       <Input type="text" placeholder="Sök" />
